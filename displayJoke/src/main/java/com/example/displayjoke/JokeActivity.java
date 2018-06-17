982306c845b9a520d0f4b38f25753c4745a1abd4
Agostino Coppolino <agostone.tiscali.it>
2018-06-17 22:16:59 +0200

    Lint optimization

--- displayJoke/src/main/java/com/example/displayjoke/JokeActivity.java
+++ displayJoke/src/main/java/com/example/displayjoke/JokeActivity.java
@@ -1,11 +1,8 @@
 package com.example.displayjoke;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class JokeActivity extends AppCompatActivity {
 
@@ -15,17 +12,9 @@ public class JokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
